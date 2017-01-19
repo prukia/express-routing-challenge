279bbb5f4938dd3ac8fe3d20a093b032f9fa2c37
Rukia Sheikh-mohamed <rukiasheikh-mohamed@macbook-2.local>
2017-01-19 06:48:39 -0600

    completed steps 1 and 2

---
+++ public/client.js
@@ -0,0 +1,41 @@
+$(function (){
+  console.log('Document loaded');
+
+  getSongs();
+  $('#addSong').on('submit', addSong);
+});
+
+function getSongs(){
+  $.ajax({
+    url: '/songs',
+    type: 'GET',
+    success: displaySongs
+
+  });
+}
+function addSong(event){
+  //stop the browser from trying to navigate away from our page
+  //only needs this to prevent the default web browser function
+  //usually just used for forms.
+  event.preventDefault();
+  //get the information out of the form
+  var songData = $(this).serialize();
+  console.log(songData);
+
+  $.ajax({
+    url: '/songs',
+    type: 'POST',
+    data: songData,
+    sucess: getSongs
+  })
+}
+ function displaySongs(songs){
+   console.log(songs);
+
+   $('#songs').empty();
+   
+   songs.forEach(function(song){
+     $('#songs').append('<li>' + song.title + ' by ' +
+     song.artist + ' from album ' + song.album + '</li>');
+   });
+ }
---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var path = require ('path');
+var bodyParser = require('body-parser');
+
+var songs = require('./data.json');
+
+var app = express();
+
+app.use(express.static('public'));
+//static is a method in express
+//enables us to serve static files
+
+//covert any url encoded body into a JS object
+// added to req.body
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/', function (req, res){
+  res.sendFile(path.join (__dirname, 'public/index.html'));
+  //handling a get request.
+
+});
+
+app.get('/songs', function (req, res){
+  res.send(songs);
+
+});
+
+app.post('/songs', function (req, res){
+  console.log('req.body', req.body);
+  //puts object into the array (called songs)
+  if(req.body.title == '' || req.body.artist == '' || req.body.albums == '' || duplicateCheck(req.body)){
+    res.sendStatus(400);
+  }else{
+    songs.push(req.body);
+    res.sendStatus(200);
+  }
+
+})
+
+function duplicateCheck(double){
+  var duplicate;
+  songs.forEach(function(song){
+    if(song.title == double.title){
+    duplicate = true;
+    console.log(duplicate);
+  }else{
+    duplicate = false;
+  }
+  })
+  return duplicate;
+};
+app.listen(3000);
